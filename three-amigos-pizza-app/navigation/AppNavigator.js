f7dc2dea528fa22810c8af1b09af3a067d6aae74
jonahgolden <jonahwgolden@gmail.com>
2019-12-01 23:11:40 -0800

    checkout screen

--- three-amigos-pizza-app/navigation/AppNavigator.js
+++ three-amigos-pizza-app/navigation/AppNavigator.js
@@ -3,11 +3,13 @@ import { createAppContainer, createStackNavigator } from 'react-navigation';
 
 import { MenuScreen } from '../screens/MenuScreen';
 import { StoreScreen } from '../screens/StoreScreen';
+import { CheckoutScreen } from '../screens/CheckoutScreen';
 
 export default createAppContainer(
   createStackNavigator({
     // You could add another route here for authentication.
     // Read more at https://reactnavigation.org/docs/en/auth-flow.html
+    Checkout: CheckoutScreen,
     Store: StoreScreen,
     Menu: MenuScreen
   })
