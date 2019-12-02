e7020cf0d2559fdb9a8231ca63c44630fd0798d3
jonahgolden <jonahwgolden@gmail.com>
2019-12-02 13:28:55 -0800

    Customer component?

---
+++ three-amigos-pizza-app/components/Customer.js
@@ -0,0 +1,18 @@
+// import React from 'react';
+
+// export class Customer extends React.Component {
+//     constructor(props) { 
+//         super(props);
+//     }
+
+//     const customer = new Customer(
+//         this.state.name,
+//         this.state.phone,
+//         this.state.address,
+//         this.state.creditCardNumber,
+//         this.state.cvv,
+//         this.state.expirationMonth,
+//         this.state.expirationYear,
+//     );
+
+// }
